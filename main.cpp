fddd6f17017e6ce9b8eed49e954d5ffd3f2a5890
ArkChoi <ark.goya@gmail.com>
2025-09-16 11:19:51 +0900

    2 code plus

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <windows.h>
 
 using namespace std;
 
 int main() 
 {
-	int PlayerTankHP = 10;
+	// 1번 연습
+	/*int PlayerTankHP = 10;
 	int PlayerTankAttack = 5;
 	int PlayerTankDefence = 3;
 
@@ -20,7 +22,22 @@ int main()
 	cout << "EnemyTankAttack!" << endl;
 	PlayerTankHP = PlayerTankHP - EnemyDamege;
 
-	cout << "PlayerTankHP : " << PlayerTankHP << endl;
+	cout << "PlayerTankHP : " << PlayerTankHP << endl;*/
+
+	// 2번 연습
+	int PlayerX = 0;
+	int PlayerY = 0;
+
+	PlayerX++;
+	PlayerY++;
+
+	COORD Cur;
+	Cur.X = PlayerX;
+	Cur.Y = PlayerY;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
+
+	cout << "@" << endl;
+
 
 	return 0;
 }
